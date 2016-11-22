934524b1df8eb8da5ec23c785be21f0e889ddf24
robson Cassol <robsoncassol@gmail.com>
2016-11-22 10:42:40 -0200

    organização inicial do projeto

---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="container">
+                    
+      </footer>
+    );
+  }
+}
+
+export default Menu;
---
+++ src/layout/SideMenu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SideMenu extends Component {
+  render() {
+    return (
+      <nav className="container">
+        
+      </nav>
+    );
+  }
+}
+
+export default SideMenu;
